4a4216e77d7c2c054d966d67d1936190f2305f3e
wangwei17 <wb-ww794567@antgroup.com>
2020-09-29 13:46:55 +0800

    Mybatis-learning resultMap

---
+++ mybatis-resultMap/src/main/java/com/doublew/domain/QueryVo.java
@@ -0,0 +1,16 @@
+package com.doublew.domain;
+
+/**
+ */
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
