fd55c7baeaf125941c74f7eaab75af43dde579a4
wangwei17 <wb-ww794567@antgroup.com>
2020-10-02 10:56:31 +0800

    Mybatis-learning manytomany

---
+++ mybatis-many2many/src/main/java/com/doublew/dao/IUserDao.java
@@ -0,0 +1,23 @@
+package com.doublew.dao;
+
+import com.doublew.domain.User;
+
+import java.util.List;
+
+public interface IUserDao {
+
+    /**
+     * 查询所有操作
+     * @return
+     */
+    List<User> findAll();
+
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+
+}
